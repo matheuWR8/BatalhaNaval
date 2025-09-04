cc53345a6025d57f2078984b268bf3aea7fcb919
matheuWR8 <matheusswr8@gmail.com>
2025-09-04 16:28:21 -0300

    Implementação dos tipos de tiro no padrão strategy

---
+++ src/main/java/br/edu/cefsa/batalhanaval/Model/TiroDuplo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.cefsa.batalhanaval.Model;
+
+/**
+ *
+ * @author math
+ */
+public class TiroDuplo implements Tiro {
+    @Override
+    public void disparar(Tabuleiro tabuleiro, int x, int y){
+        if (tabuleiro.validarPosicao(x, y) && tabuleiro.validarPosicao(x + 1, y)) {
+            tabuleiro.getCelula(x,y).atingir();
+            tabuleiro.getCelula(x + 1, y).atingir();
+        }
+    }
+}
---
+++ src/main/java/br/edu/cefsa/batalhanaval/Model/TiroExplosivo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.cefsa.batalhanaval.Model;
+
+/**
+ *
+ * @author math
+ */
+public class TiroExplosivo implements Tiro {
+
+    @Override
+    public void disparar(Tabuleiro tabuleiro, int x, int y) {
+        if (!tabuleiro.validarPosicao(x, y)) {
+            return;
+        }
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (tabuleiro.validarPosicao(x + i, y + j)) {
+                    tabuleiro.getCelula(x, y).atingir();
+                }
+            }
+        }
+    }
+}
